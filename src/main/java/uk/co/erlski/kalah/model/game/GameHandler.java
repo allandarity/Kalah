e60fc443d540be3a457da4adb7ae6065cdd013e5
elliott <elliott@elliott.lan>
2019-07-09 21:03:28 +0100

    add missed additions

--- src/main/java/uk/co/erlski/kalah/model/game/GameHandler.java
+++ src/main/java/uk/co/erlski/kalah/model/game/GameHandler.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import uk.co.erlski.kalah.exception.KalahException;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Holds all running instances of a {@link Game}.
@@ -15,24 +16,28 @@ public class GameHandler {
     public GameHandler() {
     }
 
-    /*
-    Todo:
-        clear finished games out of the map so the id can be reused/less chance of collision
-     */
-
     /**
      * Map  holding all running instances of games
      */
-    private HashMap<Long, Game> games = new HashMap<>();
+    private Map<Long, Game> games = new HashMap<>();
 
     /**
-     * Adds a game, if it doesn't already exist, to the {@link this#games} map
+     * Adds a game, if it doesn't already exist, to the {@link this#games} map.
+     * If the game already exists it checks to see whether the {@link GameState}
+     * is set to finished - if yes, it replaces it.
      * @param gameId Unique identifier of the {@link Game} to be created
      * @throws KalahException exception
      */
     public void addGame(final Long gameId) throws KalahException {
+        if(gameId < 0) {
+            throw new KalahException("ID is too low");
+        }
         if(games.containsKey(gameId)) {
-            throw new KalahException("Game already exists");
+            if(getValidGame(gameId).getGameState().equals(GameState.LIVE)) {
+                throw new KalahException("Game already exists and is live");
+            } else {
+                removeFinishedGame(gameId);
+            }
         }
         games.put(gameId, new Game(gameId));
         startGame(getValidGame(gameId));
@@ -65,6 +70,34 @@ public class GameHandler {
             throw new KalahException("Error. A game with the id " + gameId +" doesn't exist");
         }
         return games.get(gameId);
+    }
+
+    /**
+     * Check whether a {@link Game} has finished yet by checking the {@link Game#getGameState()}
+     * If the game state returns finished the instance is removed from the map and replaced with
+     * a new one.
+     * @param gameId
+     * @return
+     * @throws KalahException
+     */
+    public boolean removeFinishedGame(final Long gameId) throws KalahException {
+        if(gameId == null) {
+            throw new KalahException("Error. You have not specified a gameId");
+        }
+        if(!games.containsKey(gameId)) {
+            throw new KalahException("Error. A game with the id " + gameId +" doesn't exist");
+        }
+        Game game = getValidGame(gameId);
+        if(!game.getGameState().equals(GameState.FINISHED)) {
+            throw new KalahException("Error. A game with the id " + gameId +" hasn't finished yet");
+        }
+        games.remove(gameId);
+        return true;
+    }
+
+
+    public Map<Long, Game> getGamesMap() {
+        return games;
     }
 
 }
--- src/test/java/uk/co/erlski/kalah/model/game/GameHandlerTest.java
+++ src/test/java/uk/co/erlski/kalah/model/game/GameHandlerTest.java
@@ -1,5 +1,92 @@
+package uk.co.erlski.kalah.model.game;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.erlski.kalah.exception.KalahException;
+
 import static org.junit.Assert.*;
 
 public class GameHandlerTest {
 
+    private GameHandler gameHandler;
+    @Before
+    public void setUp() throws Exception {
+        gameHandler = new GameHandler();
+        gameHandler.addGame(1L);
+    }
+
+    @Test
+    public void addGame() {
+        gameHandler.addGame(2L);
+        Assert.assertNotNull(gameHandler.getValidGame(1L));
+        Assert.assertNotNull(gameHandler.getValidGame(2L));
+    }
+
+    @Test(expected = KalahException.class)
+    public void addGameWithSameId() {
+        Assert.assertNotNull(gameHandler.getValidGame(1L));
+        gameHandler.addGame(1L);
+    }
+
+    @Test
+    public void addGameByReplacingId() {
+        Long gameId = 1L;
+        Game oldGame = gameHandler.getValidGame(gameId);
+
+        oldGame.setGameState(GameState.FINISHED);
+        gameHandler.addGame(gameId);
+        Game newGame = gameHandler.getValidGame(gameId);
+        assertEquals(GameState.LIVE, newGame.getGameState());
+    }
+
+    @Test(expected = KalahException.class)
+    public void failAddGameByAttemptedReplacementId() {
+        Long gameId = 1L;
+        Game oldGame = gameHandler.getValidGame(gameId);
+
+        oldGame.setGameState(GameState.LIVE);
+        gameHandler.addGame(gameId);
+    }
+
+
+    @Test(expected = KalahException.class)
+    public void addGameWithNegativeID() {
+        gameHandler.addGame(-5L);
+    }
+
+    @Test
+    public void getValidGame() {
+        assertNotNull(gameHandler.getValidGame(1L));
+    }
+
+    @Test(expected = KalahException.class)
+    public void attemptToGetGameWithNullID() {
+        gameHandler.getValidGame(null);
+    }
+
+    @Test(expected = KalahException.class)
+    public void attemptToGetGameThatDoesntExist() {
+        gameHandler.getValidGame(3L);
+    }
+
+    @Test
+    public void removeFinishedGame() {
+        Game game = gameHandler.getValidGame(1L);
+        game.setGameState(GameState.FINISHED);
+        assertTrue(gameHandler.removeFinishedGame(1L));
+    }
+
+    @Test
+    public void checkMapRemovedFinishedGame() {
+        Game game = gameHandler.getValidGame(1L);
+        game.setGameState(GameState.FINISHED);
+        gameHandler.removeFinishedGame(1L);
+        assertFalse(gameHandler.getGamesMap().containsKey(1L));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
